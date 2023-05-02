750da0e6fe623c3829a8afb39fe47adc3974e116
Kyle Givler <xvegan88x@gmail.com>
2023-05-02 07:33:17 -0400

    Use Binding

---
+++ TheBookOfFSharpChapter1/UseBinding/Program.fs
@@ -0,0 +1,22 @@
+﻿open System
+
+let createDisposable name = 
+    printfn "Creating: %s" name
+    {
+        new IDisposable with
+        member x.Dispose() =
+            printfn "disposing: %s" name
+    }
+
+
+let testDisposable() =
+    use root = createDisposable "outer"
+    for i in [1..2] do
+        use nested = createDisposable (sprintf "inner %i" i)
+        printfn "completing iteration %i" i
+    printfn "leaving function"
+
+[<EntryPoint>]
+let main argv = 
+    testDisposable()
+    0
