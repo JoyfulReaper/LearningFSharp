d25f32ae08713373f026d2549b0c4b8e5f73f4db
Kyle Givler <xvegan88x@gmail.com>
2023-05-02 10:59:43 -0400

    Enum and Option example

--- TheBookOfFSharpChapter1/UseBinding/Program.fs
+++ TheBookOfFSharpChapter1/UseBinding/Program.fs
@@ -16,7 +16,12 @@ let testDisposable() =
         printfn "completing iteration %i" i
     printfn "leaving function"
 
+let approximatelyEqual(x: float) (y: float) (threshold: float) =
+    Math.Abs(x - y) <=  Math.Abs(threshold)
+
 [<EntryPoint>]
 let main argv = 
     testDisposable()
+    let result = approximatelyEqual 0.33333 (1.0 / 3.0) 0.001
+    printfn "%b" result
     0
