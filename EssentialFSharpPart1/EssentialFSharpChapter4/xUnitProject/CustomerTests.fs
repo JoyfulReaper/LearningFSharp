a9e128d8d7d68f435bc86763005b281555517f05
Kyle Givler <xvegan88x@gmail.com>
2023-07-09 17:21:38 -0400

    Chapter 4 - Part 1

---
+++ EssentialFSharpPart1/EssentialFSharpChapter4/xUnitProject/CustomerTests.fs
@@ -0,0 +1,21 @@
+﻿namespace MyProjectTests
+
+open Xunit
+open Myproject.Customer
+
+(* Code from earlier in the chapter
+namespace MyProjectTests
+
+open System
+open Xunit
+
+module ``I can group my tests in a module and run`` =
+    
+    [<Fact>]
+    let ``My first test`` () =
+        Assert.True(true)
+
+    [<Fact>]
+    let ``My second test`` () =
+        Assert.True(true)
+*)
