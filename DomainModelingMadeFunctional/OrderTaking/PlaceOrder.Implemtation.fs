032cd70f4d4a5fb5b7d35f56a5dd64356d82bd73
Kyle Givler <xvegan88x@gmail.com>
2023-08-11 08:13:54 -0400

    Messing with some DDD made functional code

---
+++ DomainModelingMadeFunctional/OrderTaking/PlaceOrder.Implemtation.fs
@@ -0,0 +1,124 @@
+﻿module internal OrderTaking.PlaceOrder.ImplementationWithoutEffects
+
+open OrderTaking.Common
+
+// ======================================================
+// This file contains the implementation for the PlaceOrder workflow
+// WITHOUT any effects like Result or Async
+//
+// This represents the code in chapter 9, "Composing a Pipeline"
+//
+// There are two parts:
+// * the first section contains the (type-only) definitions for each step
+// * the second section contains the implementations for each step
+//   and the implementation of the overall workflow
+// ======================================================
+
+
+// ------------------------------------
+// the workflow itself, without effects
+
+type PlaceOrderWithoutEffects = 
+    UnvalidatedOrder -> PlaceOrderEvent list
+
+// ======================================================
+// Override the SimpleType constructors 
+// so that they raise exceptions rather than return Results
+// ======================================================
+
+// helper to convert Results into exceptions so we can reuse the smart constructors in SimpleTypes.
+let failOnError aResult =
+    match aResult with
+    | Ok success -> success
+    | Error error -> failwithf "%A" error
+
+module String50 =
+    let create fieldName = String50.create fieldName >> failOnError
+    let createOption fieldName = String50.createOption fieldName >> failOnError
+
+module EmailAddress =
+    let create fieldName = EmailAddress.create fieldName >> failOnError
+
+module ZipCode =
+    let create fieldName = ZipCode.create fieldName >> failOnError
+
+module OrderId =
+    let create fieldName = OrderId.create fieldName >> failOnError
+
+module OrderLineId =
+    let create fieldName = OrderLineId.create fieldName >> failOnError
+
+module WidgetCode =
+    let create fieldName = WidgetCode.create fieldName >> failOnError
+
+module GizmoCode =
+    let create fieldName = GizmoCode.create fieldName >> failOnError
+
+module ProductCode =
+    let create fieldName = ProductCode.create fieldName >> failOnError
+
+module UnitQuantity  =
+    let create fieldName = UnitQuantity.create fieldName >> failOnError
+
+module KilogramQuantity =
+    let create fieldName = KilogramQuantity.create fieldName >> failOnError
+
+module OrderQuantity  =
+    let create fieldName productCode = OrderQuantity.create fieldName productCode >> failOnError
+
+module Price =
+    let create = Price.create >> failOnError
+    let multiply qty price = Price.multiply qty price |> failOnError
+
+module BillingAmount =
+    let create = BillingAmount.create >> failOnError
+    let sumPrices = BillingAmount.sumPrices >> failOnError
+
+// ======================================================
+// Section 1 : Define each step in the workflow using types
+// ======================================================
+
+// ---------------------------
+// Validation step
+// ---------------------------
+
+// Product validation
+
+type CheckProductCodeExists =
+    ProductCode -> bool
+
+// Address validation Exception
+exception AddressValidationFailure of string
+
+type CheckedAddress = CheckedAddress of UnvalidatedAddress
+
+type CheckAddressExists =
+    UnvalidatedAddress -> CheckedAddress
+
+// ---------------------------
+// Validated Order 
+// ---------------------------
+
+type ValidatedOrderLine = {
+    OrderLineId : OrderLineId
+    ProductCode : ProductCode
+    Quantity : OrderQuantity
+}
+
+type ValidatedOrder = {
+    OrderId : OrderId
+    CustomerInfo : CustomerInfo
+    ShippingAddress : Address
+    BillingAddress : Address
+    Line : ValidatedOrderLine list
+}
+
+type ValidateOrder = 
+    CheckProductCodeExists  // dependency
+      -> CheckAddressExists // dependency
+      -> UnvalidatedOrder   // input
+      -> ValidatedOrder     // output
+
+// ---------------------------
+// Pricing step
+// ---------------------------
--- DomainModelingMadeFunctional/OrderTaking/Result.fs
+++ DomainModelingMadeFunctional/OrderTaking/Result.fs
@@ -1,4 +1,4 @@
-﻿namespace global // note use of GLOBAL namespace
+﻿namespace global  // note use of GLOBAL namespace
 
 open System
 
@@ -6,64 +6,42 @@ open System
 // Helpers for Result type and AsyncResult type
 //==============================================
 
-(*
-/// The Result type represents a choice between success and failure
-type Result<'success, 'failure> = 
-    | Ok of 'success
-    | Error of 'failure
-*)
-
 /// Functions for Result type (functor and monad).
 /// For applicatives, see Validation.
 [<RequireQualifiedAccess>]  // RequireQualifiedAccess forces the `Result.xxx` prefix to be used
 module Result =
-    
+
     /// Pass in a function to handle each case of `Result`
-    let bimap onSuccess onError xR =
-       match xR with
+    let bimap onSuccess onError xR = 
+        match xR with
         | Ok x -> onSuccess x
-        | Error e -> onError e
+        | Error err -> onError err
 
-    (*
-    let map f result = 
-        match result with
-        | Ok success -> Ok (f success)
-        | Error err -> Error err 
-
-    let mapError f result = 
-        match result with
-        | Ok success -> Ok success
-        | Error err -> Error (f err)
-
-    let bind f result = 
-        match result with
-        | Ok success -> f success
-        | Error err -> Error err
-    *)
     let map = Result.map
     let mapError = Result.mapError
     let bind = Result.bind
 
     // Like `map` but with a unit-returning function
-    let iter (f : _ -> unit) result =
-        map f result |> ignore
+    let iter (f : _ -> unit) result = 
+        map f result |> ignore    
 
     /// Apply a Result<fn> to a Result<x> monadically
-    let apply fR xR =
+    let apply fR xR = 
         match fR, xR with
         | Ok f, Ok x -> Ok (f x)
         | Error err1, Ok _ -> Error err1
         | Ok _, Error err2 -> Error err2
-        | Error err1, Error _ -> Error err1
+        | Error err1, Error _ -> Error err1 
 
-    // combine list of result, monadically
-    let sequence aListOfResults =
-        let (<*>) = apply
+
+    // combine a list of results, monadically
+    let sequence aListOfResults = 
+        let (<*>) = apply // monadic
         let (<!>) = map
         let cons head tail = head::tail
         let consR headR tailR = cons <!> headR <*> tailR
         let initialValue = Ok [] // empty list inside Result
-
+ 
         // loop through the list, prepending each element
         // to the initial value
         List.foldBack consR aListOfResults initialValue
@@ -72,24 +50,24 @@ module Result =
     // Lifting
 
     /// Lift a two parameter function to use Result parameters
-    let lift2 f x1 x2 =
+    let lift2 f x1 x2 = 
         let (<!>) = map
         let (<*>) = apply
         f <!> x1 <*> x2
 
     /// Lift a three parameter function to use Result parameters
-    let lift3 f x1 x2 x3 =
+    let lift3 f x1 x2 x3 = 
         let (<!>) = map
         let (<*>) = apply
         f <!> x1 <*> x2 <*> x3
 
     /// Lift a four parameter function to use Result parameters
-    let lift4 f x1 x2 x3 x4 =
+    let lift4 f x1 x2 x3 x4 = 
         let (<!>) = map
         let (<*>) = apply
         f <!> x1 <*> x2 <*> x3 <*> x4
 
-    /// Apply a monadic function with two parameters
+    /// Apply a monadic function with two parameters 
     let bind2 f x1 x2 = lift2 f x1 x2 |> bind id
 
     /// Apply a monadic function with three parameters
@@ -99,57 +77,60 @@ module Result =
     // Predicates
 
     /// Predicate that returns true on success
-    let isOk =
-        function
+    let isOk = 
+        function 
         | Ok _ -> true
         | Error _ -> false
 
     /// Predicate that returns true on failure
-    let isError xR =
+    let isError xR = 
         xR |> isOk |> not
 
     /// Lift a given predicate into a predicate that works on Results
-    let filter pred =
-        function
+    let filter pred = 
+        function 
         | Ok x -> pred x
         | Error _ -> true
+
 
     //-----------------------------------
     // Mixing simple values and results
 
     /// On success, return the value. On error, return a default value
-    let ifError defaultValue =
-        function
+    let ifError defaultVal = 
+        function 
         | Ok x -> x
-        | Error _ -> defaultValue
+        | Error _ -> defaultVal
+
 
     //-----------------------------------
     // Mixing options and results
 
-    /// Apply a monadic function to a Result<x option>
+    /// Apply a monadic function to an Result<x option>
     let bindOption f xR =
         match xR with
         | Some x -> f x |> map Some
         | None -> Ok None
 
-    /// Convert an Option into a Result. If none, use the passed-in errorValue
-    let ofOption errorValue opt =
+    /// Convert an Option into a Result. If none, use the passed-in errorValue 
+    let ofOption errorValue opt = 
         match opt with
         | Some v -> Ok v
         | None -> Error errorValue
 
-    /// Convert a Result into an Option
-    let toOption xR =
+    /// Convert a Result into an Option 
+    let toOption xR = 
         match xR with
         | Ok v -> Some v
         | Error _ -> None
 
-    /// Convert the Error case into an option
-    /// (useful with List.choose to find all error in a list of Results)
-    let toErrorOption =
-        function
+    /// Convert the Error case into an Option 
+    /// (useful with List.choose to find all errors in a list of Results)
+    let toErrorOption = 
+        function 
         | Ok _ -> None
         | Error err -> Some err
+
 
 //==============================================
 // Computation Expression for Result
@@ -157,11 +138,11 @@ module Result =
 
 [<AutoOpen>]
 module ResultComputationExpression =
-    
+
     type ResultBuilder() =
         member __.Return(x) = Ok x
         member __.Bind(x, f) = Result.bind f x
-
+    
         member __.ReturnFrom(x) = x
         member this.Zero() = this.Return ()
 
@@ -169,10 +150,10 @@ module ResultComputationExpression =
         member __.Run(f) = f()
 
         member this.While(guard, body) =
-            if not (guard())
-            then this.Zero()
-            else this.Bind(body(), fun () ->
-                this.While(guard, body))
+            if not (guard()) 
+            then this.Zero() 
+            else this.Bind( body(), fun () -> 
+                this.While(guard, body))  
 
         member this.TryWith(body, handler) =
             try this.ReturnFrom(body())
@@ -180,21 +161,21 @@ module ResultComputationExpression =
 
         member this.TryFinally(body, compensation) =
             try this.ReturnFrom(body())
-            finally compensation()
+            finally compensation() 
 
         member this.Using(disposable:#System.IDisposable, body) =
             let body' = fun () -> body disposable
-            this.TryFinally(body', fun () ->
-                match disposable with
-                    | null -> ()
+            this.TryFinally(body', fun () -> 
+                match disposable with 
+                    | null -> () 
                     | disp -> disp.Dispose())
 
         member this.For(sequence:seq<_>, body) =
-            this.Using(sequence.GetEnumerator(), fun enum ->
-                this.While(enum.MoveNext,
+            this.Using(sequence.GetEnumerator(),fun enum -> 
+                this.While(enum.MoveNext, 
                     this.Delay(fun () -> body enum.Current)))
 
-        member this.Combine (a, b) =
+        member this.Combine (a,b) = 
             this.Bind(a, fun () -> b())
 
     let result = new ResultBuilder()
@@ -205,97 +186,101 @@ module ResultComputationExpression =
 // by combining their errors ("applicative-style")
 //==============================================
 
-type Validation<'Sucess, 'Failure> =
-    Result<'Sucess, 'Failure list>
+type Validation<'Success,'Failure> = 
+    Result<'Success,'Failure list>
 
-/// Functions for the 'Validation' type (mostly applicative)
-[<RequireQualifiedAccess>] // RequireQualifiedAccess forces the `Validation.xxx` prefix to be used
+/// Functions for the `Validation` type (mostly applicative)
+[<RequireQualifiedAccess>]  // RequireQualifiedAccess forces the `Validation.xxx` prefix to be used
 module Validation =
-    
-    /// Apply a Validation<fn> to a Validation<x> applicativly
-    let apply (fV:Validation<_,_>) (xV:Validation<_,_>) : Validation<_,_> =
+
+    /// Apply a Validation<fn> to a Validation<x> applicatively
+    let apply (fV:Validation<_,_>) (xV:Validation<_,_>) :Validation<_,_> = 
         match fV, xV with
         | Ok f, Ok x -> Ok (f x)
-        | Error err1, Ok _ -> Error err1
-        | Ok _, Error err2 -> Error err2
+        | Error errs1, Ok _ -> Error errs1
+        | Ok _, Error errs2 -> Error errs2
         | Error errs1, Error errs2 -> Error (errs1 @ errs2)
 
-    // Combine a list of Validation, applicatively
-    let sequence (aListOfValidations:Validation<_,_> list) =
+    // combine a list of Validation, applicatively
+    let sequence (aListOfValidations:Validation<_,_> list) = 
         let (<*>) = apply
         let (<!>) = Result.map
-        let cons head tail = head :: tail
+        let cons head tail = head::tail
         let consR headR tailR = cons <!> headR <*> tailR
         let initialValue = Ok [] // empty list inside Result
-
-        // loop through the list, prepending eash element
+  
+        // loop through the list, prepending each element
         // to the initial value
         List.foldBack consR aListOfValidations initialValue
 
     //-----------------------------------
     // Converting between Validations and other types
-
-    let ofResult xR : Validation<_,_> =
+    
+    let ofResult xR :Validation<_,_> = 
         xR |> Result.mapError List.singleton
 
-    let toResult (xV:Validation<_,_>) : Result<_,_> =
+    let toResult (xV:Validation<_,_>) :Result<_,_> = 
         xV
+
+
 
 //==============================================
 // Async utilities
 //==============================================
 
-[<RequireQualifiedAccess>]
+[<RequireQualifiedAccess>]  // RequireQualifiedAccess forces the `Async.xxx` prefix to be used
 module Async =
 
-    /// Lift function to Async
-    let map f xA =
-        async {
-            let! x = xA
-            return f x
+    /// Lift a function to Async
+    let map f xA = 
+        async { 
+        let! x = xA
+        return f x 
         }
 
-    /// Lift value to Async
-    let retn x =
+    /// Lift a value to Async
+    let retn x = 
         async.Return x
 
-    /// Apply an Async function to an Async value
-    let apply fA xA =
-        async {
-            // start the two asyncs in parallel
-            let! fChild = Async.StartChild fA // run in parallel
-            let! x = xA
-            // wait for the result of the first one
-            let! f = fChild
-            return f x
+    /// Apply an Async function to an Async value 
+    let apply fA xA = 
+        async { 
+         // start the two asyncs in parallel
+        let! fChild = Async.StartChild fA  // run in parallel
+        let! x = xA
+        // wait for the result of the first one
+        let! f = fChild
+        return f x 
         }
 
-    /// Apply a monadic function to an Async value
-    let bind f xA = async.Bind(xA, f)
+    /// Apply a monadic function to an Async value  
+    let bind f xA = async.Bind(xA,f)
+
 
 //==============================================
 // AsyncResult
 //==============================================
 
-type AsyncResult<'Success, 'Failure> =
-    Async<Result<'Success, 'Failure>>
+type AsyncResult<'Success,'Failure> = 
+    Async<Result<'Success,'Failure>>
 
-[<RequireQualifiedAccess>]
+[<RequireQualifiedAccess>]  // RequireQualifiedAccess forces the `AsyncResult.xxx` prefix to be used
 module AsyncResult =
 
     /// Lift a function to AsyncResult
     let map f (x:AsyncResult<_,_>) : AsyncResult<_,_> =
         Async.map (Result.map f) x
 
+    /// Lift a function to AsyncResult
     let mapError f (x:AsyncResult<_,_>) : AsyncResult<_,_> =
         Async.map (Result.mapError f) x
 
-    /// Apply ignore to then internal value
-    let ignore x =
-        x |> map ignore
+    /// Apply ignore to the internal value
+    let ignore x = 
+        x |> map ignore    
 
     /// Lift a value to AsyncResult
-    let retn x : AsyncResult<_,_> =
+    let retn x : AsyncResult<_,_> = 
         x |> Result.Ok |> Async.retn
 
     /// Handles asynchronous exceptions and maps them into Failure cases using the provided function
@@ -305,10 +290,11 @@ module AsyncResult =
         |> Async.map(function
             | Choice1Of2 (Ok v) -> Ok v
             | Choice1Of2 (Error err) -> Error err
-            | Choice2Of2 ex -> Error (f x))
+            | Choice2Of2 ex -> Error (f ex))
+
 
     /// Apply an AsyncResult function to an AsyncResult value, monadically
-    let applyM (fAsyncResult : AsyncResult<_,_>) (xAsyncResult : AsyncResult<_, _>) :AsyncResult<_,_> = 
+    let applyM (fAsyncResult : AsyncResult<_, _>) (xAsyncResult : AsyncResult<_, _>) :AsyncResult<_,_> = 
         fAsyncResult |> Async.bind (fun fResult ->
         xAsyncResult |> Async.map (fun xResult -> Result.apply fResult xResult))
 
@@ -325,16 +311,20 @@ module AsyncResult =
         | Error err -> return (Error err)
         }
 
+
     /// Convert a list of AsyncResult into a AsyncResult<list> using monadic style. 
     /// Only the first error is returned. The error type need not be a list.
-    let sequenceM resultList =
+    let sequenceM resultList = 
         let (<*>) = applyM
         let (<!>) = map
         let cons head tail = head::tail
         let consR headR tailR = cons <!> headR <*> tailR
-        let initialValue = retn[]
+        let initialValue = retn [] // empty list inside Result
+  
+        // loop through the list, prepending each element
+        // to the initial value
+        List.foldBack consR resultList  initialValue
 
-        List.foldBack consR resultList initialValue
 
     /// Convert a list of AsyncResult into a AsyncResult<list> using applicative style. 
     /// All the errors are returned. The error type must be a list.
@@ -374,6 +364,7 @@ module AsyncResult =
     let sleep ms = 
         (Async.Sleep : int -> Async<unit>) ms |> ofAsync
 
+    
 // ==================================
 // AsyncResult computation expression
 // ==================================
